074224a8cbe65aad191cdce870fc225284cc96f9
rupesh9999 <rupesh.b500@gmail.com>
2025-12-17 09:09:50 +0530

    Add createCategory method to CategoryService interface

--- src/main/java/com/ecommerce/project/service/CategoryService.java
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -1,12 +1,14 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.payload.CategoryDTO;
 import com.ecommerce.project.payload.CategoryResponse;
 
-import java.util.List;
+
 
 public interface CategoryService {
     CategoryResponse getAllCategories();
+    CategoryDTO createCategory(CategoryDTO categoryDTO);
     void createCategory(Category category);
 
     String deleteCategory(Long categoryId);

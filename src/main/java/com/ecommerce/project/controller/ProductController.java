04e6707f4ea655c662a067032c965b1ce550d5ec
rupesh9999 <rupesh.b500@gmail.com>
2025-12-20 18:40:24 +0530

    Add endpoint to retrieve products by category in ProductController

--- src/main/java/com/ecommerce/project/controller/ProductController.java
+++ src/main/java/com/ecommerce/project/controller/ProductController.java
@@ -31,4 +31,10 @@ public class ProductController {
           return new ResponseEntity<>(productResponse, HttpStatus.OK);
     }
 
+    @GetMapping("/public/category/{categoryId}/products")
+    public ResponseEntity<ProductResponse> getProductsByCategory(@PathVariable Long categoryId){
+        ProductResponse productResponse =  productService.searchByCategory(categoryId);
+        return new ResponseEntity<>(productResponse, HttpStatus.OK);
+    }
+
 }

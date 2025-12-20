56f791b3b7b9325d89a78f57ba77e02004805934
rupesh9999 <rupesh.b500@gmail.com>
2025-12-20 16:17:43 +0530

    Add validation annotations to CategoryDTO for category name

--- src/main/java/com/ecommerce/project/payload/CategoryDTO.java
+++ src/main/java/com/ecommerce/project/payload/CategoryDTO.java
@@ -1,6 +1,8 @@
 package com.ecommerce.project.payload;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,5 +12,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CategoryDTO {
     private Long categoryId;
+
+    @NotBlank
+    @Size(min = 5, message = "Category name should have at least 5 characters")
     private String categoryName;
 }

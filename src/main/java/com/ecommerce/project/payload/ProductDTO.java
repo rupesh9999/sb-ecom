0ed1a677f429b3d4b67219cede26775a491265e6
rupesh9999 <rupesh.b500@gmail.com>
2025-12-20 16:34:13 +0530

    Add description and category fields to ProductDTO

--- src/main/java/com/ecommerce/project/payload/ProductDTO.java
+++ src/main/java/com/ecommerce/project/payload/ProductDTO.java
@@ -13,9 +13,12 @@ public class ProductDTO {
     private Long productId;
     private String productName;
     private String image;
+    private String description;
     private Integer quantity;
     private double price;
     private double discount;
     private double specialPrice;
+    private Long categoryId;
+    private String categoryName;
 
 }

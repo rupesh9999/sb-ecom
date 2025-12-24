5903cebe96bf1dd2004aa9abae365a7a209e12c7
rupesh9999 <rupesh.b500@gmail.com>
2025-12-24 12:33:19 +0530

    Update ProductRepository to support pagination for product name search

--- src/main/java/com/ecommerce/project/repository/ProductRepository.java
+++ src/main/java/com/ecommerce/project/repository/ProductRepository.java
@@ -7,12 +7,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findByCategoryOrderByPriceAsc(Category category, Pageable pageDetails);
 
-    List<Product> findByProductNameLikeIgnoreCase(String keyword);
+    Page<Product> findByProductNameLikeIgnoreCase(String keyword, Pageable pageDetails);
 }

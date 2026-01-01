09e4074c19aa729be645bcb290acff28c4640dff
rupesh9999 <rupesh.b500@gmail.com>
2026-01-01 12:53:27 +0530

    Add UserRepository interface for user data access

---
+++ src/main/java/com/ecommerce/project/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.ecommerce.project.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserName(String username);
+}

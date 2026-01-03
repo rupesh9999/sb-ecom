eca1bd48d429821ff27065d314cd187a81bf40da
rupesh9999 <rupesh.b500@gmail.com>
2026-01-03 21:34:47 +0530

    Refactor UserDetailsServiceImpl to remove unused imports and improve code clarity

--- src/main/java/com/ecommerce/project/security/services/UserDetailsServiceImpl.java
+++ src/main/java/com/ecommerce/project/security/services/UserDetailsServiceImpl.java
@@ -1,10 +1,9 @@
 package com.ecommerce.project.security.services;
 
+import com.ecommerce.project.model.User;
 import com.ecommerce.project.repositories.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;

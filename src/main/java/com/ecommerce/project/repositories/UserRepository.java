314b27b0614b573b3d95d4535d67587818128fab
rupesh9999 <rupesh.b500@gmail.com>
2026-01-03 15:57:13 +0530

    Refactor UserRepository to import User model from the correct package

--- src/main/java/com/ecommerce/project/repositories/UserRepository.java
+++ src/main/java/com/ecommerce/project/repositories/UserRepository.java
@@ -1,11 +1,11 @@
 package com.ecommerce.project.repositories;
 
 
+import com.ecommerce.project.model.User;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;

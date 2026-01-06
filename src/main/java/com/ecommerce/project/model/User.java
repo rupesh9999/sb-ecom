7f431aee4fc0791d5009c5be990acde636939484
rupesh9999 <rupesh.b500@gmail.com>
2026-01-06 07:52:11 +0530

    Add one-to-one relationship between User and Cart entities

--- src/main/java/com/ecommerce/project/model/User.java
+++ src/main/java/com/ecommerce/project/model/User.java
@@ -65,4 +65,9 @@ public class User {
     @ToString.Exclude
     @OneToMany(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
     private Set<Product> products;
+
+    @ToString.Exclude
+    @OneToOne(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
+    private Cart cart;
+
 }

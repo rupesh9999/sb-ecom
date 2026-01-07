d08ac4d226db6980efbd61096dc37974f90b2af5
rupesh9999 <rupesh.b500@gmail.com>
2026-01-07 11:30:14 +0530

    Add endpoints to CartController for retrieving all carts and user's cart by email

--- src/main/java/com/ecommerce/project/controller/CartController.java
+++ src/main/java/com/ecommerce/project/controller/CartController.java
@@ -1,15 +1,17 @@
 package com.ecommerce.project.controller;
 
 
+import com.ecommerce.project.model.Cart;
 import com.ecommerce.project.payload.CartDTO;
+import com.ecommerce.project.repositories.CartRepository;
 import com.ecommerce.project.service.CartService;
+import com.ecommerce.project.util.AuthUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -18,11 +20,33 @@ public class CartController {
     @Autowired
     private CartService cartService;
 
+    @Autowired
+    CartRepository cartRepository;
+
+    @Autowired
+    private AuthUtil authUtil;
+
 
 
     @PostMapping("/carts/products/{productId}/quantity/{quantity}")
     public ResponseEntity<CartDTO> addProductToCart(@PathVariable Long productId, @PathVariable Integer quantity) {
         CartDTO cartDTO = cartService.addProductToCart(productId, quantity);
         return new ResponseEntity<CartDTO>(cartDTO, HttpStatus.CREATED);
+    }
+
+
+    @GetMapping("/carts")
+    public ResponseEntity<List<CartDTO>> getCart() {
+        List<CartDTO> cartDTO = cartService.getAllCarts();
+        return new ResponseEntity<List<CartDTO>>(cartDTO, HttpStatus.OK);
+    }
+
+    @GetMapping("/carts/users/cart")
+    public ResponseEntity<CartDTO> getCartById(){
+          String emailId = authUtil.loggedInEmail();
+          Cart cart = cartRepository.findCartByEmail(emailId);
+          Long cartId = cart.getCartId();
+          CartDTO cartDTO = cartService.getCart(emailId, cartId);
+          return new ResponseEntity<CartDTO>(cartDTO, HttpStatus.OK);
     }
 }

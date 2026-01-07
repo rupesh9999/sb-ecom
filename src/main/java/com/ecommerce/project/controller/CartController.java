98b38d8a2157b67dbc6d96b300831b83cbcc4ec5
rupesh9999 <rupesh.b500@gmail.com>
2026-01-07 16:08:56 +0530

    Add endpoint to update product quantity in cart

--- src/main/java/com/ecommerce/project/controller/CartController.java
+++ src/main/java/com/ecommerce/project/controller/CartController.java
@@ -49,4 +49,10 @@ public class CartController {
           CartDTO cartDTO = cartService.getCart(emailId, cartId);
           return new ResponseEntity<CartDTO>(cartDTO, HttpStatus.OK);
     }
-}
+
+    @PutMapping("/cart/products/{productId/quantity/{operation}")
+    public ResponseEntity<CartDTO> updateCart(@PathVariable Long productId, @PathVariable String operation) {
+        CartDTO cartDTO = cartService.updateProductQuantityInCart(productId, operation.equalsIgnoreCase("delete") ? -1 : 1);
+        return new ResponseEntity<CartDTO>(cartDTO, HttpStatus.OK);
+    }
+ }
